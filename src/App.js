697c3c63feea6d6b70aedba68c072c9d7c707af4
Steven Haye <steven@northwestmediacollective.com>
2022-06-15 16:41:23 -0700

    added style to cards, added layout component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupsPage from "./pages/NewMeetups";
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
 
   return (
-    <div className="App">
-      <MainNavigation/>
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} exact/>
         <Route path="/new-meetup" element={<NewMeetupsPage />}/>   
         <Route path="/favorites" element={ <FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
